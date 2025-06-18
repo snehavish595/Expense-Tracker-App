27711f70507558387d317ed8c4ef57539368b34d
Sneha Vishwakarma <88725034+snehavish595@users.noreply.github.com>
2025-06-18 19:53:13 +0530

    header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="flex flex-wrap w-full h-12">
+      <div className="left w-1/10 bg-green-200">
+        <Image
+          className="h-12 w-12 mx-auto rounded-3xl p-1 border border-amber-500"
+          src="/images/logo.png"
+          alt="Logo"
+          width={200}
+          height={20}
+        />
+      </div>
+      <div className="middle w-4/5 bg-green-300"></div>
+      <div className="right w-1/10 bg-green-400"></div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
